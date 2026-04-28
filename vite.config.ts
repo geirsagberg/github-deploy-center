5d9b2b4e32ccca0ee409a6ddc784edd1c3fdeb51
Geir Sagberg <geir.sagberg@gmail.com>
2026-04-28 15:04:27 +0200

    Update Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), visualizer()],
-  resolve: {
-    alias: {
-      'node-fetch': 'isomorphic-fetch',
+export default defineConfig(async () => {
+  const { visualizer } = await import('rollup-plugin-visualizer')
+
+  return {
+    plugins: [react(), visualizer()],
+    resolve: {
+      alias: {
+        'node-fetch': 'isomorphic-fetch',
+      },
     },
-  },
-  build: {
-    emptyOutDir: true,
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          mui: ['@mui/material', '@emotion/react', '@emotion/styled'],
+    build: {
+      emptyOutDir: true,
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (
+              id.includes('/node_modules/@mui/material/') ||
+              id.includes('/node_modules/@emotion/react/') ||
+              id.includes('/node_modules/@emotion/styled/')
+            ) {
+              return 'mui'
+            }
+          },
         },
       },
     },
-  },
+  }
 })
